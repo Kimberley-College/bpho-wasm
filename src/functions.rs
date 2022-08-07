1caefec679baa3214967eb4fab4692c3460df0e7
Nick <hello@nicholasg.me>
2022-08-07 23:17:52 +0100

    *chore: convert to f32 *chore: change return to boxed slice
    Co-authored-by: Jamie <JacquesRW@users.noreply.github.com>

--- src/functions.rs
+++ src/functions.rs
@@ -1,10 +1,10 @@
 //* Important functions */
 // calculating relative humidity
-pub fn calc_es(t: f64) -> f64 { 6.1121 * ((18.678 - t / 234.5) * (t / (t + 257.14))).exp() }
+pub fn calc_es(t: f32) -> f32 { 6.1121 * ((18.678 - t / 234.5) * (t / (t + 257.14))).exp() }
 // calculating dp/dh
-pub fn calc_dp(p: f64, ues: f64, tk: f64) -> f64 { -34.171 * (p - 0.37776 * ues) / tk }
+pub fn calc_dp(p: f32, ues: f32, tk: f32) -> f32 { -34.171 * (p - 0.37776 * ues) / tk }
 // calculating lapse rate
-pub fn calc_l(p: f64, ues: f64, tk: f64) -> f64 {
+pub fn calc_l(p: f32, ues: f32, tk: f32) -> f32 {
     let r = ues / (p-ues);
     9.7734 * (tk + 5420.3 * r) / (tk * tk + 8400955.5 * r) * tk
 }
--- src/lib.rs
+++ src/lib.rs
@@ -12,15 +12,15 @@ static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 // Runge-Kutta 4th Order
 #[wasm_bindgen]
-pub fn rk4(u: f64) -> *const [[f64; 111];3] {
+pub fn rk4(u: f32) -> Box<[f32]> {
     // stack allocations
-    let (mut p, mut t, mut l) : ([f64; 111],[f64; 111],[f64; 111]) = ([0.0;111],[0.0;111],[0.0;111]);
-    let (mut t1, mut t2, mut t3, mut t4) : (f64, f64, f64, f64);
-    let (mut p1, mut p2, mut p3, mut p4) : (f64, f64, f64, f64);
-    let (mut ues1, mut ues2, mut ues3, mut ues4) : (f64, f64, f64, f64);
-    let (mut tk1, mut tk2, mut tk3, mut tk4) : (f64, f64, f64, f64);
-    let (mut kt1, mut kt2, mut kt3, mut kt4) : (f64, f64, f64, f64);
-    let (mut kp1, mut kp2, mut kp3, mut kp4) : (f64, f64, f64, f64);
+    let (mut p, mut t, mut l) : ([f32; 111],[f32; 111],[f32; 111]) = ([0.0;111],[0.0;111],[0.0;111]);
+    let (mut t1, mut t2, mut t3, mut t4) : (f32, f32, f32, f32);
+    let (mut p1, mut p2, mut p3, mut p4) : (f32, f32, f32, f32);
+    let (mut ues1, mut ues2, mut ues3, mut ues4) : (f32, f32, f32, f32);
+    let (mut tk1, mut tk2, mut tk3, mut tk4) : (f32, f32, f32, f32);
+    let (mut kt1, mut kt2, mut kt3, mut kt4) : (f32, f32, f32, f32);
+    let (mut kp1, mut kp2, mut kp3, mut kp4) : (f32, f32, f32, f32);
     // initial conditions
     p[0] = P0;
     t[0] = T0;
@@ -60,26 +60,32 @@ pub fn rk4(u: f64) -> *const [[f64; 111];3] {
         p[i] = p1 + (kp1 + 2.0 * kp2 + 2.0 * kp3 + kp4) * DH / 6.0;
         l[i] = kt1;
     }
-    let soln: [[f64;111];3] = [p,t,l];
-    std::ptr::addr_of!(soln)
+    [p,t,l].concat().into_boxed_slice()
  }
 
  // Euler's Method
  #[wasm_bindgen]
- pub fn euler(u: f64) -> *const [[f64; 1101];3] {
-    let (mut p, mut t, mut l) : ([f64; 1101],[f64; 1101],[f64; 1101]) = ([0.0;1101],[0.0;1101],[0.0;1101]);
-    let (mut t1, mut ues1, mut tk1) : (f64, f64, f64);
+ pub fn euler(u: f32) -> Box<[f32]> {
+    let (mut p, mut t, mut l) : ([f32; 111],[f32; 111],[f32; 111]) = ([0.0;111],[0.0;111],[0.0;111]);
+    let (mut t1, mut ues1, mut tk1, mut tm, mut pm, mut lm) : (f32, f32, f32, f32, f32, f32);
     p[0] = P0;
     t[0] = T0;
     l[0] = calc_l(P0, u * calc_es(T0), T0 + KELVIN);
-    for i in 1..1101 {
-        t1 = t[i - 1] - l[i - 1] * DHE;
-        ues1 = u * calc_es(t1);
-        tk1 = t1 + 273.15;
-        t[i] = t1;
-        p[i] = p[i - 1] + DHE * calc_dp(p[i - 1], ues1, tk1);
-        l[i] = calc_l(p[i], ues1, tk1);
+    for i in 1..111 {
+        tm = t[i-1];
+        lm = l[i-1];
+        pm = p[i-1];
+        for _ in 0..10 {
+            t1 = tm - lm * DHE;
+            ues1 = u * calc_es(t1);
+            tk1 = t1 + KELVIN;
+            tm = t1;
+            pm = pm + DHE * calc_dp(pm, ues1, tk1);
+            lm = calc_l(pm, ues1, tk1);
+        }
+        t[i] = tm;
+        l[i] = lm;
+        p[i] = pm;
     }
-    let soln: [[f64;1101];3] = [p,t,l];
-    std::ptr::addr_of!(soln)
+    [p,t,l].concat().into_boxed_slice()
 }
