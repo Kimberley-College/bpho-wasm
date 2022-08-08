7f8c338afb7796b4780aa4d1cae887ee5d40d184
Jacques <jamie.whiting2003@gmail.com>
2022-08-08 01:21:45 +0100

    shoved everything into one file

--- src/consts.rs
+++
@@ -1,7 +0,0 @@
-//* IMPORTANT CONSTANTS */
-
-pub const P0: f32 = 1013.25; // initial pressure
-pub const T0: f32 = 15.0; // initial temperature
-pub const DH: f32 = 0.1; // step size for rk4
-pub const DHE: f32 = 0.01; // step size for euler
-pub const KELVIN: f32 = 273.15; // for celcius -> kelvin
